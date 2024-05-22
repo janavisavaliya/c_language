dd20c3614f272baa77b95af5349dcc6809b5cf2e
Janavi savaliya <164509498+janavisavaliya@users.noreply.github.com>
2024-05-22 09:17:24 +0530

    Update p1.c

--- file handling/p1.c
+++ file handling/p1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+void main()
+{
+	FILE *fp;
+	FILE *fp2;
+	
+	fp = fopen("janavi.txt","r");
+	
+	if(fp!=NULL)
+	{
+		char msg[20];
+		
+		fscanf(fp,"%[^\n]",&msg);
+		
+		fp2 = fopen("write.txt","w");
+		
+		if(fp2!=NULL)
+		{
+			fprintf(fp2,"%s",msg);
+		}
+	}
+	else
+	{
+		printf("File is Not Open...........");
+	}
+}
